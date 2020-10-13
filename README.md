b228fb9f2158d8ddeee852f244c736a13aaede79
demsheng <sheng0619@163.com>
2020-10-13 16:28:20 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,32 @@
+## QtVtkHelloWorld
+
+- 作者：李长圣
+- 邮箱：sheng0619@163.com
+- 功能：演示如何编译运行 `qt` 和 `vtk` 程序。
+- 支持 `window10` 和 `linux` 系统
+
+![预览] (showcase.png）
+
+### 编译运行
+使用 `qtcreator` 打开 `QtVtkHelloWorld.pro` 编译运行
+
+### window10 安装 qt 和 vtk
+
+1. http://download.qt.io/archive/qt 下载安装Qt。   
+本实例使用的版本 `Qt 5.12.9`
+2. https://vtk.org/download/下载 vtk  
+本实例使用的版本为 `VTK-8.2.0.zip` ，编译安装vtk 参考：https://sheng.geovbox.com/dem/vtk/。  
+    - `cmake` 编译器选择64位
+    - 编译目录
+        1. VTK-8.2.0-src `源码`
+        2. VTK-8.2.0-build `构建目录`
+        3. VTK-8.2.0-Install-VS2017-x64-Release `CMAKE_INSTALL_PREFIX`
+        4. VTK-8.2.0-Install-VS2017-x64-Debug `CMAKE_INSTALL_PREFIX`
+    - cmake 注意勾选中 `VTK_Group_Qt`
+    - `Qt5_DIR` 为 `D:/Qt/5.12.9/msvc2017_64/lib/cmake/Qt5`
+    - **编译vtk** cmake Generate 之后， 去 `VTK-8.2.0-build` 中 使用 `visual stdio 2017` 打开 `VTK.sln`，选择【生成】-->【批生成】，在ALL_BUILD  Release x64 栏 勾选 √---->点击【生成】
+    - **安装vtk** 把刚刚勾选的ALL_BUILD Release后面的√去掉，下拉，在INSTALL Release后面勾选
+3. 配置 `QtVtkHelloWorld.pro`，见源码。
+4. 打开 `qtcreator` 编译运行项目 `QtVtkHelloWorld.pro` 。
+
+**注意：** qtcreator 中一定要选择 Desktop_Qt_5_12_9_MSVC2017_64bit 编译器， `Release` 版本。否则，会出不明原因错误。 
---
+++ main.cpp
@@ -0,0 +1,174 @@
+﻿#include "vtkAutoInit.h"
+VTK_MODULE_INIT(vtkRenderingOpenGL2);  // VTK was built with vtkRenderingOpenGL2
+VTK_MODULE_INIT(vtkInteractionStyle);
+VTK_MODULE_INIT(vtkRenderingFreeType);
+
+#include <iostream>
+//VTK includes
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkLODActor.h>
+#include <vtkAssembly.h>
+#include <vtkSTLReader.h>
+#include <vtkProperty.h>
+#include <vtkCamera.h>
+#include <vtkPlaneSource.h>
+#include <vtkTransform.h>
+#include <vtkAxesActor.h>
+#include <vtkCaptionActor2D.h>
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCylinderSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkSphereSource.h>
+
+#include <QVTKOpenGLWidget.h>
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QToolButton>
+#include <QSurfaceFormat>
+#include <QDebug>
+
+int vtk_with_qt(int argc, char* argv[]);
+
+void setGround();
+void setAxes();
+void setCamera();
+
+vtkSmartPointer<vtkRenderer> renderer;
+vtkSmartPointer<vtkGenericOpenGLRenderWindow> renWin;
+
+//主函数
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+    return vtk_with_qt(argc, argv);
+}
+
+int vtk_with_qt(int argc, char* argv[]){
+
+    QApplication a(argc, argv);
+
+    QWidget w;// = new QWidget(0);
+    QVBoxLayout layout;// = new QVBoxLayout(0);
+    QVTKOpenGLWidget v;// = new QVTKOpenGLWidget();
+    //v.setFixedSize(300, 300);
+    layout.addWidget(&v);
+    w.setLayout(&layout);
+
+    //layout.setSpacing(0);//设置布局内的布局与布局之间或控件与控件之间的距离
+    //layout.setContentsMargins(0, 0, 0, 0);//设置主窗口dialog与此主布局之间的margin
+
+    //给主窗口设置 尺寸策略，为了内部布局可以跟着窗口一起扩展，需要用“Expanding”
+    QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    w.setSizePolicy(sizePolicy);
+    w.setMinimumSize(QSize(500, 500));
+
+// 1. 建立renderer
+    renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->SetBackground(0.1, 0.2, 0.4);
+
+// 2. 建立圆柱
+//    auto cylinder = vtkSmartPointer<vtkCylinderSource>::New();
+//    cylinder->SetHeight(2.0);
+//    cylinder->SetRadius(1.0);
+//    cylinder->SetResolution(10);
+//    auto cylinderMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+//    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+//    auto cylinderActor = vtkSmartPointer<vtkActor>::New();
+//    cylinderActor->SetMapper(cylinderMapper);
+
+//    renderer->AddActor(cylinderActor);
+
+// 3. 建立球 sphere
+    vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetCenter(0, 1, 0);//设置球的中心
+    sphereSource->SetRadius(1.0);//设置球的半径
+    sphereSource->SetThetaResolution(30);//设置球表面精度，值越大球的光滑程度越高
+
+    vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    sphereMapper->SetInputConnection( sphereSource->GetOutputPort() );
+
+    vtkSmartPointer<vtkActor> sphereActor = vtkSmartPointer<vtkActor>::New();
+    sphereActor->SetMapper( sphereMapper );
+    sphereActor->GetProperty()->SetColor(255/255, 0/255,0/255);
+//    sphereActor->GetProperty()->SetRepresentationToWireframe();//以线框的方式显示
+    sphereActor->GetProperty()->SetRepresentationToSurface();//以表面的方式显示
+
+    renderer->AddActor( sphereActor );
+
+//4. 建立 render window
+    renWin = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    renWin->AddRenderer(renderer);
+
+    setAxes();
+    setGround();
+    setCamera();
+
+    v.SetRenderWindow(renWin);
+    w.show();
+    return a.exec();
+}
+
+void setCamera() {
+    qDebug() << "setCamera";
+    if(renderer == nullptr) {
+        qDebug() << "Renderer is nullptr";
+        return;
+    }
+
+    vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New();
+    //camera->SetFocalPoint(1, 0, 1);
+    camera->SetPosition(10, 2, 5);
+    //camera->Zoom(0.1);
+    camera->SetViewUp(0, 0, 1);
+    renderer->SetActiveCamera(camera);
+}
+
+void setGround() {
+    qDebug() << "setGround";
+    vtkSmartPointer<vtkActor> ground = vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkPlaneSource> ground_plane = vtkSmartPointer<vtkPlaneSource>::New();
+
+    ground_plane->SetXResolution(50);
+    ground_plane->SetYResolution(50);
+    ground_plane->SetCenter(0, 0, 0);
+    ground_plane->SetNormal(0, 0, 1);
+
+    vtkSmartPointer<vtkPolyDataMapper> ground_mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    ground_mapper->SetInputConnection(ground_plane->GetOutputPort());
+    ground->SetMapper(ground_mapper);
+    ground->GetProperty()->SetRepresentationToWireframe();
+    vtkSmartPointer<vtkTransform> ground_transform = vtkSmartPointer<vtkTransform>::New();
+    ground_transform->Scale(4, 4, 1);
+    ground->SetUserTransform(ground_transform);
+
+    renderer->AddActor(ground);
+
+}
+
+void setAxes() {
+    qDebug() << "setAxes";
+    if(renderer == nullptr) {
+        qDebug() << "Renderer is nullptr";
+        return;
+    }
+
+    vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
+    axes->SetTotalLength(2, 2, 2); //  # Set the total length of the axes in 3 dimensions
+    axes->SetShaftType(1);
+    axes->SetCylinderRadius(0.02);
+    axes->GetXAxisCaptionActor2D()->SetWidth(0.04);
+    axes->GetYAxisCaptionActor2D()->SetWidth(0.04);
+    axes->GetZAxisCaptionActor2D()->SetWidth(0.04);
+
+    renderer->AddActor(axes);
+}
+
+
